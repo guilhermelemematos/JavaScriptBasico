eab4b21cf2a634fb22ac6259e3f8b4fce4a39462
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-08-01 12:42:30 -0300

    Add files via upload

---
+++ Funcoes/funcoes.js
@@ -0,0 +1,12 @@
+saldo = 1000
+function dadoascesso(){
+    numcartao = prompt("Digite o numero do seu cartão:")
+    senha= prompt("Digite sua senha:")
+}
+
+function saque(valor){
+    saldo = saldo - valor
+    return saldo
+}
+
+
